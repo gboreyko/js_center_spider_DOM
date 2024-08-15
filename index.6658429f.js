560b6bf80da09fc29229ccd86b30646122fc1d93
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-08-15 18:11:27 +0300

    make build

---
+++ index.6658429f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth/2,i=e.clientHeight/2;t.style.left=l-t.offsetWidth/2+"px",t.style.top=i-t.offsetHeight/2+"px";
+//# sourceMappingURL=index.6658429f.js.map
